ab2f385aec2da445b86131d98337d4432dcac9a5
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-07 01:49:00 +0530

    project descriptions

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,25 +7,74 @@ const items = [
     id: 1,
     title: "Adventour",
     img: "/adventour-collage-2.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: (
+      <p>
+        A beautifully animated and entirely user-specific website built on{" "}
+        <b>React.js and Firebase</b> and many other CSS libraries and framework
+        for design
+      </p>
+    ),
+    techStack: [
+      "/react.svg",
+      "/firebase.svg",
+      "/bootstrap.svg",
+      "/json-server.png",
+    ],
   },
   {
     id: 2,
     title: "BlogsTour.com",
     img: "/medium-collage-1.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: (
+      <p>
+        A Complete <b>MERN Stack</b> blog website similar to <b>Medium.com</b>{" "}
+        which supports CRUD operations and have entirely user specific features
+        like comments, like, unlike, follows etc.
+      </p>
+    ),
+    techStack: [
+      "/react.svg",
+      "/mongo.svg",
+      "/chakra.png",
+      "/nodejs1.png",
+      "/redux.svg",
+    ],
   },
   {
     id: 3,
     title: "Alcazar Travel Agency",
     img: "/alcazar-collage-1.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: (
+      <p>
+        <b>User Specific Vanilla Javascript E-commerce website</b>, built on
+        HTML, CSS, Javascript, Firebase and JSON-Server
+      </p>
+    ),
+    techStack: [
+      "/html.svg",
+      "/css.svg",
+      "/js.svg",
+      "/json-server.png",
+      "/firebase.svg",
+    ],
   },
   {
     id: 4,
     title: "Instagram Clone",
     img: "/instagram-collage-1.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: (
+      <p>
+        A complete working clone of Instagram where users can Create Posts,{" "}
+        <b>Update their profiles</b>, and have all features of instagram
+      </p>
+    ),
+    techStack: [
+      "/react.svg",
+      "/nodejs1.png",
+      "/mongo.svg",
+      "/chakra.png",
+      "/redux.svg",
+    ],
   },
 ];
 
@@ -48,7 +97,14 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <div className="buttonContainer">
+              <button>See Demo</button>
+              <div className="techStack">
+                {item.techStack?.map((img, idx) => {
+                  return <img src={img} key={idx} />;
+                })}
+              </div>
+            </div>
           </motion.div>
         </div>
       </div>
